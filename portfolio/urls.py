609a1be326087dc26ed1da696d870779fa15d7d1
aelsir <ahmed.elsir.khalfalla@gmail.com>
2024-09-19 14:28:37 +0100

    Refactor portfolio URLs configuration

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -23,9 +23,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('resume.urls')),
-    path("__reload__/", include("django_browser_reload.urls")),
 ]
 
 if settings.DEBUG:
+        urlpatterns += path('__reload__/', include('django_browser_reload.urls')),
         urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
         urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
